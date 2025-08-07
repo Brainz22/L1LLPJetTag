67e41eccbca651f186867f944a197c53c7f0e5c5
Daniel Diaz <ddiaz006@ucr.edu>
2025-08-07 16:05:15 -0500

    make detaPhi work on arrays

--- src/L1LLPJetTagger/utils/kinematics.py
+++ src/L1LLPJetTagger/utils/kinematics.py
@@ -3,8 +3,4 @@ import numpy as np
 
 def delta_phi(phi1, phi2):
     dphi = phi1 - phi2
-    if dphi < -np.pi:
-        dphi += 2 * np.pi
-    elif dphi > np.pi:
-        dphi -= 2 * np.pi
-    return dphi
+    return (dphi + np.pi) % (2 * np.pi) - np.pi
