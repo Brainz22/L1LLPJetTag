17259cce27c314970deb5e2804c6b9851e893df4
Daniel Diaz <ddiaz006@ucr.edu>
2025-09-11 17:51:27 -0500

    add training script

--- scripts/run_dataForge.py
+++ scripts/run_dataForge.py
@@ -4,12 +4,12 @@ from L1LLPJetTagger.utils.config import config
 import os
 
 if __name__ == "__main__":
-    # root_path = os.path.join(config.PROJECT_ROOT, "DY_1k.root")
-    root_path = os.path.join(config.PROJECT_ROOT, "jetTuple_extended_5.root")
+    root_path = os.path.join(config.PROJECT_ROOT, "DY_1k.root")
+    # root_path = os.path.join(config.PROJECT_ROOT, "jetTuple_extended_5.root")
     out_path = os.path.join(config.PROJECT_ROOT, "data/")
     tree_name = "outnano/Jets"
-    # data_type = "DY"
-    data_type = "Sig"
+    data_type = "DY"
+    # data_type = "Sig"
 
     events = forge_h5(
         root_path=root_path, out_path=out_path, tree_name=tree_name, data_type=data_type
--- scripts/run_train.py
+++ scripts/run_train.py
@@ -1,12 +1,14 @@
 import argparse
 import os
 from L1LLPJetTagger.utils.config import config
-from L1LLPJetTagger.utils.utils import load_h5_data
+from L1LLPJetTagger.utils.loader import load_data
 
 
 def main():
     sig_path = os.path.join(config.PROJECT_ROOT, "data/Sig_train.h5")
     bkg_path = os.path.join(config.PROJECT_ROOT, "data/DY_train.h5")
+    sig_jet_path = os.path.join(config.PROJECT_ROOT, "data/Sig_jet_data.h5")
+    bkg_jet_path = os.path.join(config.PROJECT_ROOT, "data/DY_jet_data.h5")
     parser = argparse.ArgumentParser(description="Load HDF5 data")
 
     parser.add_argument(
@@ -18,13 +20,28 @@ def main():
         default=bkg_path,
         help="Path to the background data file",
     )
+    parser.add_argument(
+        "--signal_jet",
+        type=str,
+        default=sig_jet_path,
+        help="Path to the signal jet data file",
+    )
+    parser.add_argument(
+        "--background_jet",
+        type=str,
+        default=bkg_jet_path,
+        help="Path to the background jet data file",
+    )
     args = parser.parse_args()
 
-    signal_data = load_h5_data(args.signal, "jet_constituents")
-    background_data = load_h5_data(args.background, "jet_constituents")
+    signal, bkg, sig_meta, bkg_meta = load_data(
+        args.signal, args.background, args.signal_jet, args.background_jet
+    )
 
-    print("Signal data shape:", signal_data.shape)
-    print("Background data shape:", background_data.shape)
+    print("Signal data shape:", signal.shape)
+    print("Signal jet data shape:", sig_meta.shape)
+    print("Background data shape:", bkg.shape)
+    print("Background jet data shape:", bkg_meta.shape)
 
 
 if __name__ == "__main__":
