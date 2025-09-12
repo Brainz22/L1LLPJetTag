20b55e15a3915d0a6c5dddb3c2b34686972045ce
Daniel Diaz <ddiaz006@ucr.edu>
2025-09-12 12:22:22 -0500

    use argparse in dataForge runner

--- scripts/run_dataForge.py
+++ scripts/run_dataForge.py
@@ -27,7 +27,7 @@ def parse_args():
         "--data_type",
         type=str,
         default="DY",
-        help="Data type: Bkg Process or Sig",
+        help="Data type: BkgProcess or Sig",
     )
     return parser.parse_args()
 
